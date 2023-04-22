4c2a878cc4d0a0dfa8d083c51e32c515814adb38
GearChira <83659280+GearChira@users.noreply.github.com>
2023-04-22 10:23:14 +0000

    enter

--- 01-introduction-to-data-engineering/workshops/data-engineering-lifecycle/load.sh
+++ 01-introduction-to-data-engineering/workshops/data-engineering-lifecycle/load.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl -XPOST -H "Content-type: application/json" -d @dogs.json "https://getpantry.cloud/apiv1/pantry/<YOUR-PANTRY-ID>/basket/randomDogs"
+curl -XPOST -H "Content-type: application/json" -d @dogs.json "https://getpantry.cloud/apiv1/pantry/ee87ce2b-6383-4033-9239-087fa6f3c62d/basket/randomDogs"
